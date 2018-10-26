1be89d8e69760de8212ef45d4f1e9cbab67d91e6
Vitaliy Zub <booleanull@gmail.com>
2018-10-27 01:02:51 +0400

    links

--- app/src/main/java/com/booleanull/vectorway/posts/Post.kt
+++ app/src/main/java/com/booleanull/vectorway/posts/Post.kt
@@ -4,5 +4,6 @@ data class Post(
     val title : String = "Title",
     val text : String = "Some text",
     val date : String = "Date",
+    val site : String = "",
     val way : Int = 0,
     val level : Int = 0)
--- app/src/main/java/com/booleanull/vectorway/posts/PostAdapter.kt
+++ app/src/main/java/com/booleanull/vectorway/posts/PostAdapter.kt
@@ -1,6 +1,11 @@
 package com.booleanull.vectorway.posts
 
+import android.content.Intent
+import android.net.Uri
+import android.opengl.Visibility
+import android.support.design.widget.Snackbar
 import android.support.v4.content.ContextCompat
+import android.support.v7.widget.CardView
 import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
@@ -10,14 +15,29 @@ import android.widget.LinearLayout
 import android.widget.TextView
 import com.booleanull.vectorway.R
 
-class PostAdapter(val layoutInflater: LayoutInflater, val posts: List<Post>) : RecyclerView.Adapter<PostAdapter.ViewHolder>() {
 
-    val colors = arrayOf(R.color.colorRed, R.color.colorOrange, R.color.colorGreen)
-    val icons = arrayOf(R.drawable.ic_free_way, R.drawable.ic_ai_icon, R.drawable.ic_desktop_icon, R.drawable.ic_gamedev_icon,
-        R.drawable.ic_android_icon, R.drawable.ic_apple_icon, R.drawable.ic_frontend_icon, R.drawable.ic_backend_icon, R.drawable.ic_lowlevel_icon)
+class PostAdapter(val layoutInflater: LayoutInflater, val posts: List<Post>) :
+        RecyclerView.Adapter<PostAdapter.ViewHolder>() {
+
+    val colors = arrayOf(
+            R.color.colorRed,
+            R.color.colorOrange,
+            R.color.colorGreen)
+
+    val icons = arrayOf(
+            R.drawable.ic_free_way,
+            R.drawable.ic_ai_icon,
+            R.drawable.ic_desktop_icon,
+            R.drawable.ic_gamedev_icon,
+            R.drawable.ic_android_icon,
+            R.drawable.ic_apple_icon,
+            R.drawable.ic_frontend_icon,
+            R.drawable.ic_backend_icon,
+            R.drawable.ic_lowlevel_icon
+    )
 
     override fun onCreateViewHolder(p0: ViewGroup, p1: Int): ViewHolder {
-        val view : View = layoutInflater.inflate(R.layout.layout_post, p0, false)
+        val view: View = layoutInflater.inflate(R.layout.layout_post, p0, false)
         return ViewHolder(view)
     }
 
@@ -29,29 +49,55 @@ class PostAdapter(val layoutInflater: LayoutInflater, val posts: List<Post>) : R
         holder.title.text = posts[holder.adapterPosition].title
         holder.text.text = posts[holder.adapterPosition].text
         holder.date.text = posts[holder.adapterPosition].date
-        holder.back.setBackgroundColor(ContextCompat.getColor(layoutInflater.context, getColorBack(posts[holder.adapterPosition].level)))
+        holder.back.setBackgroundColor(
+                ContextCompat.getColor(
+                        layoutInflater.context,
+                        getColorBack(posts[holder.adapterPosition].level)
+                )
+        )
         holder.icon.setImageResource(getImageIcon(posts[holder.adapterPosition].way))
+
+        if(posts[holder.adapterPosition].site.equals(""))
+            holder.link.visibility = View.GONE
+        else
+            holder.link.visibility = View.VISIBLE
+
+        holder.card.setOnClickListener(object : View.OnClickListener {
+            override fun onClick(v: View?) {
+                if (!posts[holder.adapterPosition].site.equals("")) {
+                    val uriUrl = Uri.parse(posts[holder.adapterPosition].site)
+                    val intent = Intent(Intent.ACTION_VIEW,
+                            uriUrl)
+                    layoutInflater.context.startActivity(intent)
+                } else
+                    Snackbar
+                            .make(v!!, layoutInflater.context.getText(R.string.error_empty_site), Snackbar.LENGTH_SHORT)
+                            .show()
+            }
+        })
     }
 
-    private fun getColorBack(i : Int) : Int {
-        if(i in 0..2) {
+    private fun getColorBack(i: Int): Int {
+        if (i in 0..2) {
             return colors[i]
         }
         return colors[0]
     }
 
-    private fun getImageIcon(i : Int) : Int {
-        if(i in 0..8) {
+    private fun getImageIcon(i: Int): Int {
+        if (i in 0..8) {
             return icons[i]
         }
         return icons[0]
     }
 
     class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
-        val title : TextView = view.findViewById(R.id.title)
-        val text : TextView = view.findViewById(R.id.text)
-        val date : TextView = view.findViewById(R.id.date)
-        val back : LinearLayout = view.findViewById(R.id.back)
-        val icon : ImageView = view.findViewById(R.id.icon)
+        val card: CardView = view.findViewById(R.id.card)
+        val title: TextView = view.findViewById(R.id.title)
+        val text: TextView = view.findViewById(R.id.text)
+        val date: TextView = view.findViewById(R.id.date)
+        val back: LinearLayout = view.findViewById(R.id.back)
+        val icon: ImageView = view.findViewById(R.id.icon)
+        val link: ImageView = view.findViewById(R.id.link)
     }
 }
